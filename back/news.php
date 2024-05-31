eba7a2a329d8b5879455561f9cee07c1e7863c0e
miakojason <miakojason@gmail.com>
2024-05-31 16:39:07 +0800

    back+api->news ok 勝back que

---
+++ back/news.php
@@ -0,0 +1,50 @@
+<fieldset>
+    <form action="./api/news.php" method="post">
+        <table class="ct" style="margin: auto;">
+            <tr>
+                <th>編號</th>
+                <th>標題</th>
+                <th>顯示</th>
+                <th>刪除</th>
+            </tr>
+            <?php
+            $total = $News->count();
+            $div = 3;
+            $pages = ceil($total / $div);
+            $now = $_GET['p'] ?? 1;
+            $start = ($now - 1) * $div;
+            $rows = $News->all(" limit $start,$div");
+            foreach ($rows as $idx => $row) {
+            ?>
+                <tr>
+                    <td><?= $start+$idx + 1; ?>.</td>
+                    <td><?= $row['title']; ?></td>
+                    <td><input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                    <td>
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                        <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+        <div class="ct">
+            <?php
+            if ($now > 1) {
+                $prev = $now - 1;
+                echo "<a href='?do=$do&p=$prev'><</a>";
+            }
+            for ($i = 1; $i <= $pages; $i++) {
+                $fontsize = ($now == $i) ? '24px' : '16px';
+                echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'>$i</a>";
+            }
+            if ($now < $pages) {
+                $next = $now + 1;
+                echo "<a href='?do=$do&p=$next'>></a>";
+            }
+            ?>
+        </div>
+        <div class="ct"><input type="submit" value="確定修改"></div>
+    </form>
+</fieldset>
